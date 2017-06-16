2a1f5becf590d447baf5d4aa3d6348b8a5685aad
Michael Aquilina <michaelaquilina@gmail.com>
2017-06-16 14:29:36 +0100

    Fix inverted index population

--- you-should-use.plugin.zsh
+++ you-should-use.plugin.zsh
@@ -1,10 +1,12 @@
-typeset -A ialiases
-for k in "${(@k)aliases}"; do
-  v="${aliases[$k]}"
-  ialiases["$v"]="$k"
-done
+#! /bin/zsh
 
 function _check_aliases() {
+  typeset -g -A ialiases
+  for k in "${(@k)aliases}"; do
+    v="${aliases[$k]}"
+    ialiases["$v"]="$k"
+  done
+
   v="${ialiases["$1"]}"
   if [[ -n "$v" ]]; then
     echo "Alias exists for '$1'. You should use '$v'"
