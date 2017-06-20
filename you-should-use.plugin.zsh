e5a035b01ed722b94317de6e80a506c94bbbfb37
Michael Aquilina <michaelaquilina@gmail.com>
2017-06-20 10:08:36 +0100

    Add default ALL mode and BESTMATCH mode

--- you-should-use.plugin.zsh
+++ you-should-use.plugin.zsh
@@ -4,16 +4,33 @@ function _check_aliases() {
   local BOLD='\033[1m'
   local RED='\e[31m'
   local NONE='\033[00m'
-  local FOUND_ALIAS=0
+  local FOUND_ALIAS=()
   for k in "${(@k)aliases}"; do
     local v="${aliases[$k]}"
     if [[ "$1" = "$v" || "$1" = "$v "* ]]; then
-      echo "${BOLD}Found existing alias for \"$v\". You should use: \"$k\"${NONE}"
-      FOUND_ALIAS=1
+      FOUND_ALIAS+="$k"
     fi
   done
 
-  if [[ "$YSU_HARDCORE" = 1 && "$FOUND_ALIAS" = 1 ]]; then
+  local best_match=""
+  local best_match_key=""
+  for k in $FOUND_ALIAS; do
+    local v="${aliases[$k]}"
+    if [[ "${#v}" > "${#best_match}" ]]; then
+      best_match="$v"
+      best_match_key="$k"
+    fi
+
+    if [[ -z "$YSU_MODE" || "$YSU_MODE" = "ALL" ]]; then
+      echo "${BOLD}Found existing alias for \"$v\". You should use: \"$k\"${NONE}"
+    fi
+  done
+
+  if [[ "$YSU_MODE" = "BESTMATCH" && -n "$best_match" ]]; then
+      echo "${BOLD}Found existing alias for \"$best_match\". You should use: \"$best_match_key\"${NONE}"
+  fi
+
+  if [[ "$YSU_HARDCORE" = 1 && -n "$FOUND_ALIAS" ]]; then
       echo "${BOLD}${RED}You Should Use hardcore mode enabled. Use your aliases!${NONE}"
       kill -s INT $$
   fi
